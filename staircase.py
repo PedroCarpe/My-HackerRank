0972b83e879ce4309094193502d2b864d1816146
PedroCarpe <dd2426016@gmail.com>
2023-03-13 16:12:45 -0300

    Staircase

---
+++ staircase.py
@@ -0,0 +1,9 @@
+def staircase(n):
+    spaces = 0
+    for number in range(n+1):
+        spaces = n-number
+        if number >= 1:
+            print(spaces*' '+number*'#')
+
+
+staircase(6)        
