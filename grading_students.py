d2af7f429e98f9ee440300d0749c710e34f3e6ca
PedroCarpe <dd2426016@gmail.com>
2023-03-17 14:32:26 -0300

    Grading Students

---
+++ grading_students.py
@@ -0,0 +1,23 @@
+#comments to make ease of reading and refactoring
+
+def gradingStudents(grades):
+    
+    for g in grades:
+        counter = 1
+        #print(f'***{g}***')
+        
+        if g >= 38:
+            while counter*5 <= g:
+                #print(f'{counter * 5} {g}') 
+                counter += 1
+            #print(f'Here: {counter * 5}')
+
+            #print(f'Here: {counter*5}-{g} = {counter*5-g}')
+            if (counter * 5 - g) < 3:
+                index_grade = grades.index(g)
+                grades[index_grade] = counter*5           
+        #else:
+        #    print(f'Value out of range {g}')
+
+    return grades
+print(gradingStudents([73,67,38,33]))      
